a91ec6d136430bf7d7fb3c6ba4b76615132acd2c
Xabi Zubi <xabi.zubi@gmail.com>
2019-04-19 20:34:03 +0100

    Basic rotary dial reading

--- telefonoa.py
+++ telefonoa.py
@@ -10,13 +10,14 @@ def count(val):
     print("Pulse:", c)
 
 
-class RotaryDial():
+class RotaryDial(Thread):
     def __init__(self, ns_pin):
+        Thread.__init__(self)
         self.pin = ns_pin
         self.value = 0
         self.timeout_time = 500
 
-    def counter(self):
+    def run(self):
         while True:
             c = GPIO.wait_for_edge(self.pin, GPIO.RISING, bouncetime=90, timeout=self.timeout_time)
             if c is None:
@@ -30,7 +31,6 @@ class RotaryDial():
 
 
 if __name__ == '__main__':
-    global c
     HOERER_PIN = 13
     NS_PIN = 19
     GPIO.setmode(GPIO.BCM)
@@ -38,12 +38,8 @@ if __name__ == '__main__':
     GPIO.setup(HOERER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(NS_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    # Count dialed number
-    #count_pulses()
-
-    #GPIO.add_event_detect(NS_PIN, GPIO.RISING, callback=count, bouncetime=100)
     r = RotaryDial(NS_PIN)
 
-    r.counter()
+    r.start()
     while True:
         pass
