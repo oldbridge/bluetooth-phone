de295ea4c4e875751b39862df99c7f35668a775f
Xabi Zubi <xabi.zubi@gmail.com>
2019-04-21 12:44:05 +0100

    Add tone generator with pyAudio

--- telefonoa.py
+++ telefonoa.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import datetime
 import time
 from threading import Thread
+from threading import Event
 import queue as Queue
 import pyaudio
 import numpy as np
@@ -15,11 +16,13 @@ class RotaryDial(Thread):
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         self.value = 0
         self.timeout_time = 500
-        self.finish = False
+        self.finish = False #Event()
 
     def run(self):
         while not self.finish:
+            #print("waiting")
             c = GPIO.wait_for_edge(self.pin, GPIO.RISING, bouncetime=90, timeout=self.timeout_time)
+            #print("peak")
             if c is None:
                 if self.value > 0:
                     print("Detected: %d" % self.value)
@@ -29,6 +32,7 @@ class RotaryDial(Thread):
                     self.value = 0
             else:
                 self.value += 1
+            #print("Exit")
 
 
 class ReceiverStatus(Thread):
@@ -50,43 +54,53 @@ class ReceiverStatus(Thread):
 class Dialer(Thread):
     def __init__(self):
         Thread.__init__(self)
+
+        self.fs = 8000
+        tone_f = 440
+        points = 8000
+        self.duration = points / self.fs
+        times = np.linspace(0, self.duration, points, endpoint=False)
+        self.tone_samples = np.array((np.sin(times * tone_f * 2 * np.pi) + 1.0) * 127.5, dtype=np.int8).tostring()
+        self.tone = Event()
         self.finish = False
 
     def run(self):
-        while not self.finish():
-            pass
+        while True:  # Keep the thread alive forever
+            while not self.tone.is_set():  # Wait UNTIL the tone flag is set
+                pass
+            audio = pyaudio.PyAudio()
+            stream = audio.open(format=audio.get_format_from_width(1),
+                                     channels=1,
+                                     rate=self.fs,
+                                     output=True)
+            while self.tone.is_set():  # Play the tone WHILE the tone flas is set
+                stream.write(self.tone_samples)
+
+            stream.close()
+            audio.terminate()
 
 
-class Telephone:
+class Telephone(object):
     def __init__(self, num_pin, receiver_pin):
         self.receiver_pin = receiver_pin
         self.number_q = Queue.Queue()
         self.rotary_dial = RotaryDial(num_pin, self.number_q)
         self.receiver_status = ReceiverStatus(receiver_pin)
         self.dialer = Dialer()
-
-        # Initialize audio parameters
-        self.audio = pyaudio.PyAudio()
-        self.fs = 44100
-        tone_f = 440
-        duration = 1
-        self.tone_samples = (np.sin(2 * np.pi * np.arange(self.fs * duration) * tone_f / self.fs)).astype(np.float32)
-        self.stream = self.audio.open(format=pyaudio.paFloat32,
-                                      channels=1,
-                                      rate=self.fs,
-                                      output_device_index=1,
-                                      output=True)
+        self.finish = False
 
         # Start all threads
         self.rotary_dial.start()
         self.receiver_status.start()
         self.dialer.start()
 
-    def receiver_status_show(self):
-        self.stream.write(self.tone_samples)
+    def wait_tone(self):
+        while not self.finish:
+            if not self.receiver_status.receiver_down:
+                    self.dialer.tone.set()
+            else:
+                    self.dialer.tone.clear()
 
-    def dial_tone(self):
-        pass
 
     def close(self):
         self.rotary_dial.finish = True
@@ -100,7 +114,8 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
 
     t = Telephone(NS_PIN, HOERER_PIN)
-    for i in range(10):
-        t.receiver_status_show()
-        time.sleep(1)
+    try:
+        t.wait_tone()
+    except KeyboardInterrupt:
+        pass
     t.close()
