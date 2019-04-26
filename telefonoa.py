665074100d38e796d1adde961285733fbb987d6b
Xabi Zubi <xabi.zubi@gmail.com>
2019-04-26 20:07:40 +0100

    Fully working telephone

--- telefonoa.py
+++ telefonoa.py
@@ -1,10 +1,13 @@
 import RPi.GPIO as GPIO
 import datetime
+import dbus
+import dbus.mainloop.glib
+from gi.repository import GLib
+
 import time
 from threading import Thread
 from threading import Event
 import queue as Queue
-import alsaaudio
 import numpy as np
 import struct
 
@@ -16,40 +19,46 @@ class RotaryDial(Thread):
         self.number_q = number_queue
         GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         self.value = 0
-        self.timeout_time = 500
-        self.finish = False #Event()
-
-    def run(self):
-        while not self.finish:
-            #print("waiting")
-            c = GPIO.wait_for_edge(self.pin, GPIO.RISING, bouncetime=90, timeout=self.timeout_time)
-            #print("peak")
-            if c is None:
-                if self.value > 0:
-                    print("Detected: %d" % self.value)
-                    self.number_q.put(self.value)
-                    self.value = 0
-                else:
-                    self.value = 0
-            else:
-                self.value += 1
-            #print("Exit")
-
-
-class ReceiverStatus(Thread):
-    def __init__(self, receiver_pin):
-        Thread.__init__(self)
-        self.receiver_pin = receiver_pin
-        GPIO.setup(self.receiver_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        self.receiver_down = False
+        self.pulse_threshold = 0.2
         self.finish = False
+        GPIO.add_event_detect(ns_pin, GPIO.FALLING, callback=self.__increment, bouncetime=90)
+
+    def __increment(self, pin_num):
+        self.value += 1
 
     def run(self):
         while not self.finish:
-            if GPIO.input(self.receiver_pin) == GPIO.LOW:
-                self.receiver_down = False
-            else:
-                self.receiver_down = True
+            last_value = self.value
+            time.sleep(self.pulse_threshold)
+            if last_value != self.value:
+                pass
+            elif self.value != 0:
+                if self.value == 10:
+                    self.number_q.put(0)
+                else:
+                    self.number_q.put(self.value)
+                self.value = 0
+
+
+class ReceiverStatus():
+    def __init__(self, receiver_pin, phone_manager):
+        self.receiver_pin = receiver_pin
+        self.phone_manager = phone_manager
+        GPIO.setup(self.receiver_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        if GPIO.input(self.receiver_pin) is GPIO.HIGH:
+            self.receiver_down = True
+        else:
+            self.receiver_down = False
+        self.finish = False
+        GPIO.add_event_detect(self.receiver_pin, GPIO.BOTH, callback=self.receiver_changed, bouncetime=90)
+
+    def receiver_changed(self, pin_num):
+        if self.receiver_down:
+            self.receiver_down = False
+        else:
+            if self.phone_manager.call_in_progress:
+                self.phone_manager.end_call()
+            self.receiver_down = True
 
 
 class Dialer(Thread):
@@ -67,7 +76,7 @@ class Dialer(Thread):
         self.finish = False
 
         self.setDaemon(True)
-        self.device = alsaaudio.PCM()
+        #self.device = alsaaudio.PCM()
         self.device.setchannels(1)
         self.device.setformat(alsaaudio.PCM_FORMAT_S16_LE)
         self.device.setrate(self.fs)
@@ -94,42 +103,108 @@ class Dialer(Thread):
                 self.device.write(self.tone_buffer)
 
 
+class PhoneManager(object):
+    def __init__(self):
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        bus = dbus.SystemBus()
+        manager = dbus.Interface(bus.get_object('org.ofono', '/'), 'org.ofono.Manager')
+        modems = manager.GetModems()
+
+        # Take the first modem (there should be actually only one in our case)
+        modem = modems[0][0]
+        print(modem)
+        self.org_ofono_obj = bus.get_object('org.ofono', modem)
+        self.voice_call_manager = dbus.Interface(self.org_ofono_obj, 'org.ofono.VoiceCallManager')
+
+        self.call_in_progress = False
+        self._setup_dbus_loop()
+        print("Initialized")
+
+    def _setup_dbus_loop(self):
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.loop = GLib.MainLoop()
+        self._thread = Thread(target=self.loop.run)
+        self._thread.start()
+
+        self.org_ofono_obj.connect_to_signal("CallAdded", self.set_call_in_progress,
+                                             dbus_interface='org.ofono.VoiceCallManager')
+
+        self.org_ofono_obj.connect_to_signal("CallRemoved", self.set_call_ended,
+                                             dbus_interface='org.ofono.VoiceCallManager')
+
+    def set_call_in_progress(self, object, properties):
+        print("Call in progress!")
+        self.call_in_progress = True
+
+    def set_call_ended(self, object):
+        print("Call ended!")
+        self.call_in_progress = False
+
+    def end_call(self):
+        self.voice_call_manager.HangupAll()
+
+    def call(self, number, hide_id='default'):
+        try:
+            self.voice_call_manager.Dial(str(number), hide_id)
+        except Exception as e:
+            print("Cannot place the call, check format!")
+
+
 class Telephone(object):
     def __init__(self, num_pin, receiver_pin):
+        GPIO.setmode(GPIO.BCM)
         self.receiver_pin = receiver_pin
         self.number_q = Queue.Queue()
+        self.phone_manager = PhoneManager()
         self.rotary_dial = RotaryDial(num_pin, self.number_q)
-        self.receiver_status = ReceiverStatus(receiver_pin)
-        self.dialer = Dialer()
+        self.receiver_status = ReceiverStatus(receiver_pin, self.phone_manager)
+        #self.dialer = Dialer()
         self.finish = False
 
         # Start all threads
         self.rotary_dial.start()
-        self.receiver_status.start()
-        self.dialer.start()
+        #self.dialer.start()
 
-    def wait_tone(self):
+    def dialing_handler(self):
+        number = ''
         while not self.finish:
             if not self.receiver_status.receiver_down:
-                    self.dialer.tone.set()
+                    try:
+                        c = self.number_q.get(timeout=3)
+                        number += str(c)
+                        print(number)
+                    except Queue.Empty:
+                        if number is not '':
+                            print("Dialing: %s" % number)
+                            self.phone_manager.call(number)
+                            number = ''
+                        pass
+
             else:
-                    self.dialer.tone.clear()
+                pass
+                    #if self.phone_manager.call_in_progress:
+                     #   print("Hanging down call!")
+                        #self.phone_manager.call_in_progress = False
+                        #self.phone_manager.end_call()
+                    #print("Lift to dial")
 
 
     def close(self):
         self.rotary_dial.finish = True
         self.receiver_status.finish = True
-        self.dialer.finish = True
+        self.phone_manager.loop.quit()
+        GPIO.cleanup()
+        #self.dialer.finish = True
 
 
 if __name__ == '__main__':
     HOERER_PIN = 13
     NS_PIN = 19
-    GPIO.setmode(GPIO.BCM)
+
 
     t = Telephone(NS_PIN, HOERER_PIN)
     try:
-        t.wait_tone()
+        t.dialing_handler()
     except KeyboardInterrupt:
         pass
     t.close()
